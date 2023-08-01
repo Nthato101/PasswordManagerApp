6182a38ac0add8cb0e957e644af1778a7927d400
Nthato <nthato101@gmail.com>
2023-08-01 19:50:50 +0200

    Update with Search Button

--- main.py
+++ main.py
@@ -7,21 +7,23 @@ import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     password_input.delete(0, END)
-    user_password = password_input.insert(0,f"{password()}")
+    password_input.insert(0, f"{password()}")
+    user_password = password()
     pyperclip.copy(user_password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
 
 def save():
     website = website_input.get()
     email = email_input.get()
-    password = password_input.get()
+    password_entry = password_input.get()
     details = False
 
     if website == '':
         messagebox.showerror(title="Website Name Error", message="Please insert the website name.")
     elif email == '':
         messagebox.showerror(title="Email Error", message="Please insert a valid email address.")
-    elif password == '':
+    elif password_entry == '':
         messagebox.showerror(title="Password Error", message="Please insert a password.")
     else:
         details = True
@@ -29,16 +31,35 @@ def save():
     if details:
 
         correct = messagebox.askokcancel(title=f"{website}", message=f"Are these details correct? \nWebsite:{website}\n"
-                                                                     f"Email:{email}\nPassword:{password}\n")
+                                                                     f"Email:{email}\nPassword:{password_entry}\n")
         if correct:
             messagebox.showinfo(message="Details saved successfully")
             file = open("data.txt", "a")
-            file.write(f"{website} | {email} | {password}\n")
+            file.write(f"{website} | {email} | {password_entry}\n")
             website_input.delete(0, END)
             password_input.delete(0, END)
             file.close()
+# ---------------------------- Search Website ------------------------------- #
+
+
+def search():
+
+    with (open("data.txt") as file):
+        lines = file.readlines()
+        found = False
+        for line in lines:
+            words = line.strip()
+            site_name = website_input.get()
+            if site_name in line:
+                words = words.split("|")
+                messagebox.showinfo(title="Details",
+                                    message=f"Website:{words[0]}\nEmail:{words[1]}\nPassword:{words[2]}")
+                found = True
+        if not found:
+            messagebox.showinfo(title="Website Error", message=f"{site_name} login details are not saved.")
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title("(Password Manager")
@@ -48,7 +69,7 @@ window.config(padx=50, pady=50)
 # LOGO #
 
 lock = Canvas(width=200, height=200)
-lock.grid(row=0,column=1)
+lock.grid(row=0, column=1)
 image = PhotoImage(file="logo.png")
 lock.create_image(100, 100, image=image)
 
@@ -65,14 +86,14 @@ password_label.grid(row=3, column=0)
 
 # COLUMN 2 Elements #
 
-website_input = Entry(width=53)
-website_input.grid(row=1, column=1, columnspan=2)
+website_input = Entry(width=35)
+website_input.grid(row=1, column=1)
 website_input.focus()
 
 
 email_input = Entry(width=53)
 email_input.grid(row=2, column=1, columnspan=2)
-email_input.insert(0,"email@email.com")
+email_input.insert(0, "email@email.com")
 
 password_input = Entry(width=35)
 password_input.grid(row=3, column=1)
@@ -85,6 +106,9 @@ add_button.grid(row=4, column=1, columnspan=2)
 
 generate_button = Button(text="Generate Password", width=14, command=generate_password)
 generate_button.grid(row=3, column=2)
+
+search_button = Button(text="Search", width=14, command=search)
+search_button.grid(row=1, column=2)
 
 
 window.mainloop()
